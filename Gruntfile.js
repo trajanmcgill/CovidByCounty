badca3c6fc3c09c7d10b64063c82b0de58359686
Trajan McGill <code@trajanmcgill.com>
2020-04-02 01:57:05 -0500

    Finished basic project setup and copied in source files.

---
+++ Gruntfile.js
@@ -0,0 +1,292 @@
+module.exports = function(grunt)
+{
+	const buildNumberFile = "nextBuildNumber.txt";
+	const cacheBusterVariableName = "build";
+	const buildNumber = parseInt(grunt.file.read(buildNumberFile));
+	grunt.file.write(buildNumberFile, buildNumber + 1); // Update the next build number
+
+
+	function stripMinOrFull(dest, src)
+	{
+		var fullOriginalDest = dest + src,
+			testExp = /(.*)(\.min|\.full)(\..+)/,
+			testResult = testExp.exec(fullOriginalDest),
+			outputFile = (testResult === null) ? fullOriginalDest : (testResult[1] + testResult[3]);
+
+		console.log("copying " + fullOriginalDest + " to " + outputFile);
+
+		return outputFile;
+	} // end stripMinOrFull()
+
+
+	function htmlTagWithBuildNumberURL(originalTag, originalUrl, cacheBusterVariableName, buildNumber)
+	{
+		/*
+		if no server name
+			add build number at end before named anchor (if there is one)
+		else if concertjs.com server name
+			working with section after server name and trailing slash,
+			add build number at end before named anchor (if there is one)
+		else
+			don't add build number
+		*/
+		
+		let concertjsServerUrlRegExp = /^(https?:\/\/(?:www.)?concertjs.com)($|((?:\/[^#\n\r\?]+)*\/)?(?:([^#\n\r\?]+)(\?[^#\n\r\?]*)?(#.*)?)$)/,
+			finalUrl, finalTag;
+		
+		if(originalUrl.indexOf(":") < 0)
+		{
+			let namedAnchorPosition = originalUrl.indexOf("#"),
+				existingArgumentsPosition = originalUrl.indexOf("?"),
+				argumentsExist = (existingArgumentsPosition > -1),
+				newArgString = (argumentsExist ? "&" : "?") + cacheBusterVariableName + "=" + buildNumber;
+
+			if(namedAnchorPosition < 0)
+				finalUrl = originalUrl + newArgString;
+			else
+				finalUrl = originalUrl.substring(0, namedAnchorPosition) + newArgString + originalUrl.substring(namedAnchorPosition);
+			finalTag = originalTag.replace(originalUrl, finalUrl);
+		}
+		else
+		{
+			let concertjsServerUrlMatch = originalUrl.match(concertjsServerUrlRegExp);
+			if(concertjsServerUrlMatch !== null)
+			{
+				let serverUrl = concertjsServerUrlMatch[1],
+					filePath = (typeof(concertjsServerUrlMatch[3]) === "undefined" ? "" : concertjsServerUrlMatch[3]),
+					fileNameOrFinalPathSegment = (typeof(concertjsServerUrlMatch[4]) === "undefined" ? "" : concertjsServerUrlMatch[4])
+					argumentsExist = (typeof(concertjsServerUrlMatch[5]) !== "undefined"),
+					existingArgumentString = (argumentsExist ? concertjsServerUrlMatch[5] : ""),
+					namedAnchorString = (typeof(concertjsServerUrlMatch[6]) === "undefined" ? "" : concertjsServerUrlMatch[6]);
+
+				finalUrl = serverUrl
+					+ filePath
+					+ fileNameOrFinalPathSegment
+					+ existingArgumentString + (argumentsExist ? "&" : "?") + cacheBusterVariableName + "=" + buildNumber
+					+ namedAnchorString;
+				finalTag = originalTag.replace(originalUrl, finalUrl);
+			}
+			else
+				finalTag = originalTag;
+		}
+
+		return finalTag;
+	} // end htmlTagWithBuildNumberURL()
+
+
+	// Project configuration.
+	grunt.initConfig(
+		{
+			pkg: grunt.file.readJSON("package.json"),
+
+
+			eslint:
+			{
+				projectStandards:
+				{
+					options: { configFile: "eslint.projectStandards.json" },
+					files:
+					[
+						{
+							expand: true,
+							cwd: "src/",
+							src: ["**/*.js", "!**/*.template.js", "!DocTemplates/**/*"]
+						}
+					]
+				}
+			}, // end eslint task definitions
+
+
+			clean:
+			{
+				assembly: ["assembly/**/*"],
+
+				www: ["dist/**/*"],
+
+				removeAssembledOriginalJSandCSS:
+				{
+					src:
+					[
+						"assembly/**/*.js",
+						"assembly/**/*.css",
+						"!**/*.full.js",
+						"!**/*.full.css"
+					]
+				},
+
+				removeFullAndMinFiles:
+				{
+					src:
+					[
+						"dist/dev/**/*.full.*",
+						"dist/dev/**/*.min.*",
+						"dist/prod/**/*.full.*",
+						"dist/prod/**/*.min.*"
+					]
+				}
+			}, // end clean task definitions
+
+
+			copy:
+			{
+				assembleSrcFiles:
+				{
+					files: [ { expand: true, cwd: "src/", src: ["**/*"], dest: "assembly/" } ]
+				},
+
+				copyOriginalToFull:
+				{
+					files:
+					[
+						{ expand: true, cwd: "assembly/", src: ["**/*.js"], dest: "assembly/", ext: ".full.js" },
+						{ expand: true, cwd: "assembly/", src: ["**/*.css"], dest: "assembly/", ext: ".full.css" },
+					]
+				},
+
+				deployAssembledFiles:
+				{
+					files:
+					[
+						{ expand: true, cwd: "assembly/", src: "**/*", dest: "dist/dev/" },
+						{ expand: true, cwd: "assembly/", src: "**/*", dest: "dist/prod/" }
+					]
+				},
+
+				deployComponents:
+				{
+					files:
+					[
+						{ src: "node_modules/concert.js/dist/Concert.js", dest: "dist/dev/components/Concert.js" },
+						{ src: "node_modules/papaparse/papaparse.js", dest: "dist/dev/components/papaparse.js" },
+						{ src: "node_modules/vue/dist/vue.js", dest: "dist/dev/components/vue.js" },
+
+						{ src: "node_modules/concert.js/dist/Concert.min.js", dest: "dist/prod/components/Concert.js" },
+						{ src: "node_modules/papaparse/papaparse.min.js", dest: "dist/prod/components/papaparse.js" },
+						{ src: "node_modules/vue/dist/vue.min.js", dest: "dist/prod/components/vue.js" }
+					]
+				},
+
+				selectEnvironment:
+				{
+					files:
+					[
+						{ expand: true, cwd: "dist/dev/", src: "**/*.full.css", dest: "dist/dev/", rename: stripMinOrFull },
+						{ expand: true, cwd: "dist/dev/", src: "**/*.full.js", dest: "dist/dev/", rename: stripMinOrFull },
+
+						{ expand: true, cwd: "dist/prod/", src: "**/*.min.css", dest: "dist/prod/", rename: stripMinOrFull },
+						{ expand: true, cwd: "dist/prod/", src: "**/*.min.js", dest: "dist/prod/", rename: stripMinOrFull }
+					]
+				}
+			}, // end copy task defitions
+
+
+			addBuildNumbers:
+			{
+				allOutputHTML: { expand: true, cwd: "dist", src: "**/*.html" }
+			}, // end addBuildNumbers task definitions
+
+
+			cssmin:
+			{
+				minifyAssembledCSS: { expand: true, cwd: "assembly/", src: "**/*.full.css", dest: "assembly/", ext: ".min.css" }
+			}, // end cssmin task definitions
+
+
+			terser:
+			{
+				options:
+				{
+					warnings: "verbose",
+					parse: { ecma: 6 },
+					compress:
+					{
+						sequences: false,
+						typeofs: false,
+						warnings: true
+					},
+					mangle: {},
+					output: { comments: /^!/ }
+				},
+
+				minifyAssembledJS: { expand: true, cwd: "assembly/", src: ["**/*.full.js"], dest: "assembly/", ext: ".min.js" }
+			} // end terser task definitions
+	});
+	
+	
+	// Load the plugins
+	grunt.loadNpmTasks("grunt-contrib-clean");
+	grunt.loadNpmTasks("grunt-contrib-copy");
+	grunt.loadNpmTasks("grunt-contrib-cssmin");
+	grunt.loadNpmTasks("grunt-eslint");
+	grunt.loadNpmTasks("grunt-terser");
+	
+
+	// Define tasks
+	grunt.registerMultiTask(
+		"addBuildNumbers",
+		"Update <a>, <script>, and <link> tags in all HTML output files to add a build number to the linked files, for cache-busting purposes",
+		function()
+		{
+			const fileTargetExp = /<a\s+[^>]*href\s*=\s*"([^"#]+)[^"]*"[^>]*>|<link\s+[^>]*href\s*=\s*"([^"#]+)[^"]*"[^>]*>|<script\s+[^>]*src\s*=\s*"([^"#]+)[^"]*"[^>]*>/g;
+
+			this.files.forEach(
+				function (file)
+				{
+					let i, j, execResults, target, lastIndex, matchFound;
+
+					for(i = 0; i < file.src.length; i++)
+					{
+						let curFileName = file.src[i],
+							originalFileContents = grunt.file.read(curFileName),
+							newFileContents;
+
+						// grunt.log.writeln("Examining file:" + curFileName);
+
+						matchFound = false;
+						newFileContents = originalFileContents.replace(
+							fileTargetExp,
+							(match, p1, p2, p3, offset, fullString) =>
+								{
+									let target = [p1, p2, p3].find(element => typeof(element) === "string"),
+										matchReplacement = htmlTagWithBuildNumberURL(match, target, cacheBusterVariableName, buildNumber);
+									matchFound = true;
+									// grunt.log.writeln("  found entry at position " + offset + ":" + match);
+									// grunt.log.writeln("    target=" + target);
+									// grunt.log.writeln("    replacement entry=" + matchReplacement);
+									return matchReplacement;
+								});
+
+						if(matchFound)
+							grunt.file.write(curFileName, newFileContents);
+						// else
+						// 	grunt.log.writeln("  No build numbers needed in this file.");
+					}
+				}) // end forEach loop over all files
+
+				grunt.log.writeln("Processed " + this.files.length + " files. Current build number: " + buildNumber);
+		}); // end call to grunt.registerMultiTask("addBuildNumbers"...)
+
+
+	grunt.registerTask("clean_all", ["clean:assembly", "clean:www"]);
+
+	grunt.registerTask("lint_src", ["eslint:projectStandards"]);
+
+	grunt.registerTask(
+		"build_all",
+		[
+			"copy:assembleSrcFiles", // copy source files into assembly directory
+			"copy:copyOriginalToFull", // copy all .js and .css files in assembly directory to *.full.js and *.full.css
+			"clean:removeAssembledOriginalJSandCSS", // remove all original .js and css files from assembly directory
+			"terser:minifyAssembledJS", // minify all .full.js files in assembly directory into .min.js
+			"cssmin:minifyAssembledCSS", // minify all .full.css files in assembly directory into .min.css
+			"copy:deployAssembledFiles", // copy all assembly files into dev and prod directories
+			"copy:selectEnvironment",  // copy, in prod directory, *.min.js to *.js and *.min.css to *.css, and in dev directory, *.full.js to *.js and *.full.css to *.css
+			"clean:removeFullAndMinFiles", // clean all .min.css, .min.js, .full.css, and .full.js files from dev and prod directories
+			"copy:deployComponents", // copy external components into dev and prod directories
+			"addBuildNumbers:allOutputHTML" // add a build number url parameter to all src and href parameters in all the finished html files, for browser cache-busting purposes
+		]);
+	
+	grunt.registerTask("rebuild_all", ["clean_all", "build_all"]);
+
+	grunt.registerTask("default", ["lint_src", "rebuild_all"]);
+};
---
+++ src/scripts/app.js
@@ -0,0 +1,370 @@
+let view = (function()
+{
+	"use strict";
+
+	const FactType =
+		{
+			Cases: 0,
+			CasesPerCapita: 1,
+			Deaths: 2,
+			DeathsPerCapita: 3,
+			DeathsPerCase: 4
+		};
+	const DataViewType =
+		{
+			InstantaneousValue: 0,
+			GrowthAbsolute: 1,
+			GrowthLogarithmicBase: 2
+		};
+	//const DefaultFact = FactType.Cases;
+	//const DefaultDataView = DataViewType.InstantaneousValue;
+	//const DefaultAnimationRatioMSPerDay = 1000;
+	//const DefaultColorationHue = 0; // Red
+	//const DefaultScaleMax = null;
+
+
+	let loadHandlers = [], windowHandlerSet = false;
+	function whenDocumentLoaded(action)
+	{
+		if(document.readyState === "complete")
+			action();
+		else
+		{
+			loadHandlers.push(action);
+			if(!windowHandlerSet)
+			{
+				window.addEventListener(
+					"load",
+					function()
+					{
+						while(loadHandlers.length > 0)
+							loadHandlers.shift()();
+					});
+			}
+		}
+	} // end whenDocumentLoaded()
+	
+
+	function buildDataAnimation(allCountyData, fact, dataView, animationTimeRatio, colorationHue, exceededRangeColor, scaleMax)
+	{
+		const msPerDay = 1000 * 60 * 60 * 24;
+
+		if(scaleMax === null)
+			scaleMax = allCountyData.maxCaseCount; // CHANGE CODE HERE (this will not always be true)
+
+		let sequence = new Concert.Sequence();
+		sequence.setDefaults(
+			{
+				applicator: Concert.Applicators.SVG_ElementAttribute,
+				calculator: Concert.Calculators.Color,
+				easing: Concert.EasingFunctions.ConstantRate,
+				unit: null
+			});
+		
+		allCountyData.counties.forEach(
+			county =>
+			{
+				let mapElement = document.getElementById("SvgObject").getSVGDocument().getElementById("c" + county.id);
+
+				if (mapElement !== null)
+				{
+					let keyframeTimes = [0],
+					keyFrameValues = ["hsl(" + colorationHue + ", 100%, 100%)"];
+				
+					county.covid19Records.forEach(
+						(covid19Record /*, index, covid19Records*/) =>
+						{
+							let keyFrameTime = Math.round(((covid19Record.date - allCountyData.firstDate) / msPerDay + 1) * animationTimeRatio);
+							let keyFrameValue;
+
+							//let lastFactValue;
+							let currentFactValue;
+							if (fact === FactType.Cases)
+							{
+								//lastFactValue = (index < 1) ? 0 : covid19Records[index - 1].cumulativeCases;
+								currentFactValue = covid19Record.cumulativeCases;
+							}
+							else if (fact === FactType.CasesPerCapita)
+							{
+								// ADD CODE HERE
+							}
+							else if (fact === FactType.Deaths)
+							{
+								// ADD CODE HERE
+							}
+							else if (fact === FactType.DeathsPerCapita)
+							{
+								// ADD CODE HERE
+							}
+							else if (fact === FactType.DeathsPerCase)
+							{
+								// ADD CODE HERE
+							}
+							else
+								throw "Invalid fact parameter";
+							
+							if (dataView === DataViewType.InstantaneousValue)
+							{
+								if (currentFactValue <= scaleMax)
+									keyFrameValue = Concert.Calculators.Color(currentFactValue / scaleMax, keyFrameValues[0], "hsl(" + colorationHue + ", 100%, 50%)");
+								else
+									keyFrameValue = exceededRangeColor; // ADD CODE HERE: need to deal with making the shift to this discrete instead of gradual
+							}
+							else if (dataView === DataViewType.GrowthAbsolute)
+							{
+								// ADD CODE HERE
+							}
+							else if (dataView === DataViewType.GrowthLogarithmicBase)
+							{
+								// ADD CODE HERE
+							}
+
+							keyframeTimes.push(keyFrameTime);
+							keyFrameValues.push(keyFrameValue);
+						});
+				
+					if (document.getElementById("SvgObject").getSVGDocument().getElementById("c" + county.id) === null)
+						alert("null target");
+
+					sequence.addTransformations(
+						{
+							target: mapElement,
+							feature: "fill",
+							keyframes: { times: keyframeTimes, values: keyFrameValues }
+						});
+				}
+			});
+		
+		/*
+		let cook = allCountyData.c17031, dailyCaseRecords = cook.dailyCaseRecords;
+		let times = [0], values = ["hsl(" + colorationHue + ", 100%, 100%)"];
+		for(let i = 0; i < dailyCaseRecords.length; i++)
+		{
+			times.push(i * animationTimeRatio);
+
+			let currentValue = dailyCaseRecords[i].cumulativeCases;
+			if(currentValue <= scaleMax)
+			{
+				let currentColor = Concert.Calculators.Color(currentValue / scaleMax, values[0], "hsl(" + colorationHue + ", 100%, 50%)");
+				values.push(currentColor);
+			}
+			else
+				values.push(exceededRangeColor);
+		}
+		sequence.addTransformations(
+			{
+				target: document.getElementById("SvgObject").getSVGDocument().getElementById("c17031"),
+				feature: "fill",
+				keyframes: { times: times, values: values }
+			});
+		*/
+		return sequence;
+	}
+
+
+	let publicInterface =
+		{
+			FactType: FactType,
+			DataViewType: DataViewType,
+
+			whenDocumentLoaded: whenDocumentLoaded,
+			buildDataAnimation: buildDataAnimation
+		};
+	return publicInterface;
+})();
+
+
+let appLogic = (function()
+{
+	"use strict";
+
+	const FIPS_LENGTH = 5;
+
+
+	function parseDate(dateString)
+	{
+		let pieces = dateString.split("/"),
+			parsedDate = new Date(pieces[2], pieces[0], pieces[1]);
+		return parsedDate;
+	} // end parseDate()
+
+
+	function buildCountyList(csvText)
+	{
+		let parsedData =
+			Papa.parse(
+				csvText,
+				{
+					header: true,
+
+					transformHeader:
+						function(header)
+						{
+							if(header === "Id2")
+								return "fips";
+							else
+								return "population";
+						},
+
+					transform:
+						function(value, header)
+						{
+							if(header === "fips")
+								return value.padStart(FIPS_LENGTH, "0");
+							else
+								return value;
+						},
+					
+					dynamicTyping: header => (header === "population"),
+
+					skipEmptyLines: true
+				}).data;
+		
+		let allCountyData =
+			{
+				maxCaseCount: 0,
+				maxDeaths: 0,
+				firstDate: null,
+				counties: []
+			};
+		parsedData.forEach(
+			item =>
+			{
+				allCountyData.counties[parseInt(item.fips, 10)] =
+					{
+						id: item.fips,
+						population: item.population,
+						maxCaseCount: 0,
+						maxDeaths: 0,
+						covid19Records: []
+					};
+			});
+
+		return allCountyData;
+	} // end buildCountyList()
+
+
+	function storeCountyCaseData(csvText, allCountyData)
+	{
+		let parsedData = Papa.parse(
+			csvText,
+			{
+				header: true,
+
+				transformHeader: header => ((header === "fips") ? "id" : header),
+
+				transform:
+					(value, header) => ((header === "id") ? value.padStart(FIPS_LENGTH, "0") : value),
+
+				dynamicTyping: header => (header === "cases" || header === "deaths"),
+
+				skipEmptyLines: true
+			}).data;
+		parsedData.forEach(
+			item =>
+			{
+				if (item.id !== "00000")
+				{
+					let intId = parseInt(item.id, 10);
+					let currentCounty = allCountyData.counties[intId];
+
+					if (typeof currentCounty === "undefined")
+						throw "Error: No such county as " + item.id;
+					
+					let cases = item.cases, deaths = item.deaths, date = parseDate(item.date);
+
+					if (cases > allCountyData.maxCaseCount)
+						allCountyData.maxCaseCount = cases;
+					if (deaths > allCountyData.maxDeaths)
+						allCountyData.maxDeaths = deaths;
+					if (allCountyData.firstDate === null || date < allCountyData.firstDate)
+						allCountyData.firstDate = date;
+					
+					if (cases > currentCounty.maxCaseCount)
+						currentCounty.maxCaseCount = cases;
+					if (deaths > currentCounty.maxDeaths)
+						currentCounty.maxDeaths = deaths;
+					currentCounty.covid19Records.push(
+						{
+							date: date,
+							cumulativeCases: item.cases,
+							cumulativeDeaths: item.deaths
+						});
+					}
+			});
+	} // end storeCountyCaseData()
+
+
+	let publicInterface =
+		{
+			buildCountyList: buildCountyList,
+			storeCountyCaseData: storeCountyCaseData
+		};
+	
+	return publicInterface;
+})();
+
+
+let app = new Vue(
+	{
+		el: "#Everything",
+
+		data:
+			{
+				waitMessage: "Loading Map...",
+				waitMessageDisplay: "block",
+				allCountyData: null
+			},
+
+		mounted: function()
+		{
+			"use strict";
+
+			let runSetup =
+				(function(vueObject)
+				{
+					let setupFunction =
+						function()
+						{
+							vueObject.waitMessage = "Loading County Population Data...";
+							fetch("data/countyPopulations2018.csv")
+								.then(
+									response =>
+									{
+										vueObject.waitMessage = "Processing County Population Data...";
+										return response.text();
+									})
+								.then(
+									text =>
+									{
+										vueObject.allCountyData = appLogic.buildCountyList(text);
+										vueObject.waitMessage = "Loading County Case Data...";
+										return fetch("data/countyCases.csv");
+									})
+								.then(
+									response =>
+									{
+										vueObject.waitMessage = "Processing County Case Data...";
+										return response.text();
+									})
+								.then(
+									text =>
+									{
+										appLogic.storeCountyCaseData(text, vueObject.allCountyData);
+				
+										// CHANGE CODE: coloration test
+										window.allCountyData = vueObject.allCountyData;
+										window.sequence = view.buildDataAnimation(
+											vueObject.allCountyData,
+											view.FactType.Cases,
+											view.DataViewType.InstantaneousValue,
+											200, 0, "hsl(50, 100%, 50%)", 100);
+				
+										vueObject.waitMessageDisplay = "none";
+									});
+						};
+					return setupFunction;
+				})(this);
+			view.whenDocumentLoaded(runSetup);
+		}
+	});
