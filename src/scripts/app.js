b8f1923e80806752a1f76eeb1616faa47276eedf
Trajan McGill <code@trajanmcgill.com>
2020-04-04 11:29:51 -0500

    Made animation more step-like rather than continuous.
    Closes: #6

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -161,7 +161,8 @@ let appLogic = (function()
 	"use strict";
 
 	const TimelineDateBoxWidth = 90, TimelineStartingOffset = 541;
-	const DefaultAnimationTimeRatio = 200;
+	const DefaultAnimationTimeRatio = 500;
+	const TimelineSlideTime = 100;
 
 	let animationTimeRatio = DefaultAnimationTimeRatio;
 	let totalDays = 0;
@@ -190,6 +191,7 @@ let appLogic = (function()
 	} // end whenDocumentLoaded()
 	
 
+	// CHANGE CODE HERE: Problem to fix: animation smoothly transitions from point 0 to its first real value, even when that is many days into the animation
 	function buildDataAnimation(allCountyData, fact, dataView, colorationHue, exceededRangeColor, scaleMax)
 	{
 		const svgObject = document.getElementById("SvgObject"),
@@ -202,6 +204,7 @@ let appLogic = (function()
 			BtnSeekEnd = document.getElementById("BtnSeekEnd"),
 			AnimationSlider = document.getElementById("AnimationSlider");
 		const msPerDay = 1000 * 60 * 60 * 24;
+		const UnknownValueColor = "hsl(0, 0%, 0%)";
 
 		if(scaleMax === null)
 			scaleMax = allCountyData.maxCaseCount; // CHANGE CODE HERE (this will not always be true)
@@ -210,15 +213,17 @@ let appLogic = (function()
 		sequence.setDefaults(
 			{
 				applicator: Concert.Applicators.SVG_ElementAttribute,
-				calculator: Concert.Calculators.Color,
+				calculator: Concert.Calculators.Discrete,
 				easing: Concert.EasingFunctions.ConstantRate,
 				unit: null
 			});
 		
+		// Animate map
 		allCountyData.counties.forEach(
 			county =>
 			{
 				let mapElement = svgDocument.getElementById("c" + county.id);
+				let countyPopulation = county.population;
 
 				if (mapElement !== null)
 				{
@@ -226,51 +231,55 @@ let appLogic = (function()
 					keyFrameValues = ["hsl(" + colorationHue + ", 100%, 100%)"];
 				
 					county.covid19Records.forEach(
-						(covid19Record /*, index, covid19Records*/) =>
+						(covid19Record, index, covid19Records) =>
 						{
 							let keyFrameTime = Math.round(((covid19Record.date - allCountyData.firstDate) / msPerDay + 1) * animationTimeRatio);
 							let keyFrameValue;
 
-							//let lastFactValue;
-							let currentFactValue;
+							let lastFactValue, currentFactValue, displayFactValue;
 							if (fact === FactType.Cases)
 							{
-								//lastFactValue = (index < 1) ? 0 : covid19Records[index - 1].cumulativeCases;
+								lastFactValue = (index < 1) ? 0 : covid19Records[index - 1].cumulativeCases;
 								currentFactValue = covid19Record.cumulativeCases;
 							}
 							else if (fact === FactType.CasesPerCapita)
 							{
-								// ADD CODE HERE
+								lastFactValue = (index < 1) ? 0 : covid19Records[index - 1].cumulativeCases / countyPopulation;
+								currentFactValue = (countyPopulation > 0) ? covid19Record.cumulativeCases / countyPopulation : "unknown";
 							}
 							else if (fact === FactType.Deaths)
 							{
-								// ADD CODE HERE
+								lastFactValue = (index < 1) ? 0 : covid19Records[index - 1].cumulativeDeaths;
+								currentFactValue = covid19Record.cumulativeDeaths;
 							}
 							else if (fact === FactType.DeathsPerCapita)
 							{
-								// ADD CODE HERE
+								lastFactValue = (index < 1) ? 0 : covid19Records[index - 1].cumulativeDeaths / countyPopulation;
+								currentFactValue = (countyPopulation > 0) ? covid19Record.cumulativeDeaths / countyPopulation : "unknown";
 							}
 							else if (fact === FactType.DeathsPerCase)
 							{
-								// ADD CODE HERE
+								lastFactValue = (index < 1) ? 0 : covid19Records[index - 1].cumulativeDeaths / covid19Records[index - 1].cumulativeCases;
+								currentFactValue = (covid19Record.cumulativeCases > 0) ? covid19Record.cumulativeDeaths / covid19Record.cumulativeCases : 0;
 							}
 							else
 								throw "Invalid fact parameter";
 							
-							if (dataView === DataViewType.CumulativeValue)
+							if(currentFactValue === "unknown")
+								keyFrameValue = UnknownValueColor;
+							else
 							{
-								if (currentFactValue <= scaleMax)
-									keyFrameValue = Concert.Calculators.Color(currentFactValue / scaleMax, keyFrameValues[0], "hsl(" + colorationHue + ", 100%, 50%)");
+								if (dataView === DataViewType.CumulativeValue)
+									displayFactValue = currentFactValue;
+								else if (dataView === DataViewType.GrowthAbsolute)
+									displayFactValue = currentFactValue - lastFactValue;
+								else if (dataView === DataViewType.GrowthLogarithmicBase)
+									displayFactValue = (currentFactValue - lastFactValue) / lastFactValue;
+
+								if (displayFactValue <= scaleMax)
+									keyFrameValue = Concert.Calculators.Color(displayFactValue / scaleMax, keyFrameValues[0], "hsl(" + colorationHue + ", 100%, 50%)");
 								else
 									keyFrameValue = exceededRangeColor; // ADD CODE HERE: need to deal with making the shift to this discrete instead of gradual
-							}
-							else if (dataView === DataViewType.GrowthAbsolute)
-							{
-								// ADD CODE HERE
-							}
-							else if (dataView === DataViewType.GrowthLogarithmicBase)
-							{
-								// ADD CODE HERE
 							}
 
 							keyframeTimes.push(keyFrameTime);
@@ -289,32 +298,44 @@ let appLogic = (function()
 				}
 			}); // end forEach on allCountyData.counties
 
-		totalDays = Math.round((allCountyData.lastDate - allCountyData.firstDate) / msPerDay);
+		// Animate timeline
+		totalDays = Math.round((allCountyData.lastDate - allCountyData.firstDate + msPerDay) / msPerDay);
+		let timelineTimes = [0], timelinePositions = [TimelineStartingOffset];
+		for (let i = 0; i < totalDays; i++)
+		{
+			let nextLandingTime = (i + 1) * animationTimeRatio;
+
+			timelineTimes.push(nextLandingTime - TimelineSlideTime);
+			timelinePositions.push(TimelineStartingOffset - i * TimelineDateBoxWidth);
+
+			timelineTimes.push(nextLandingTime);
+			timelinePositions.push(TimelineStartingOffset - (i + 1) * TimelineDateBoxWidth);
+		}
 		sequence.addTransformations(
 			{
 				target: document.getElementById("Timeline"),
 				feature: "margin-left",
 				applicator: Concert.Applicators.Style,
 				calculator: Concert.Calculators.Linear,
+				easing: Concert.EasingFunctions.Smoothstep,
 				unit: "px",
-				keyframes:
-					{
-						times: [0, totalDays * animationTimeRatio],
-						values: [TimelineStartingOffset, TimelineStartingOffset - totalDays * TimelineDateBoxWidth]
-					}
+				keyframes: { times: timelineTimes, values: timelinePositions }
 			});
+		
+		// Animate slider 
+		let sliderTimes = [0], sliderPositions = [0];
+		for (let i = 1; i <= totalDays; i++)
+		{
+			sliderTimes.push(i * animationTimeRatio);
+			sliderPositions.push(i)
+		}
 		sequence.addTransformations(
 			{
 				target: document.getElementById("AnimationSlider"),
 				feature: "value",
 				applicator: Concert.Applicators.Property,
-				calculator: Concert.Calculators.Linear,
-				calculatorModifiers: { roundFactor: 1 },
-				keyframes:
-					{
-						times: [0, totalDays * animationTimeRatio],
-						values: [0, totalDays]
-					}
+				calculator: Concert.Calculators.Discrete,
+				keyframes: { times: sliderTimes, values: sliderPositions }
 			});
 		
 		BtnSeekStart.onclick = animationSeekStart;
@@ -347,7 +368,7 @@ let appLogic = (function()
 	function buildTimelineData(firstDate, lastDate, vueAppObject)
 	{
 		let currentDate = firstDate;
-		while (currentDate < lastDate)
+		while (currentDate <= lastDate)
 		{
 			let month = currentDate.toLocaleString("en-us", { month: "long" }),
 				dayOfMonth = currentDate.getDate();
@@ -381,7 +402,7 @@ let appLogic = (function()
 	function animationSeekToSpecificDay(dayNumber)
 	{
 		sequence.stop();
-		sequence.seek(dayNumber * animationTimeRatio);
+		sequence.seek(Math.min(Math.max(dayNumber, 0), totalDays) * animationTimeRatio);
 	}
 
 	function animationToggleStartStop()
@@ -474,12 +495,12 @@ let appLogic = (function()
 											buildTimelineData(allCountyData.firstDate, allCountyData.lastDate, vueObject);
 					
 											// CHANGE CODE: coloration test
-											window.allCountyData = allCountyData;
+											window.allCountyData = allCountyData; // REMOVE CODE HERE
 											buildDataAnimation(
 												allCountyData,
-												FactType.Cases,
+												FactType.Deaths,
 												DataViewType.CumulativeValue,
-												0, "hsl(50, 100%, 50%)", 100);
+												0, "hsl(50, 100%, 50%)", 1);
 					
 											vueObject.waitMessageDisplay = "none";
 										});
